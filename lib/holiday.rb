2cbbf397337572a78ec340cb1a1ba164f9c6b80a
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 18:17:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -106,7 +106,9 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     holidays.each do |holiday, itemArray|
       itemArray.each do |item|
-        holiday_hash[seasons][holiday] if item == "BBQ"
+         if item == "BBQ" then
+           holiday_hash[seasons][holiday]
+         end
       end
     end
   end
