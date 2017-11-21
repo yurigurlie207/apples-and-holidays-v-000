7f46f80ffff4fd4b6549f4c7f7f1cecae08ccc58
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-21 18:14:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -103,4 +103,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+  holiday_hash.each do |seasons, holidays|
+  end
+
 end
